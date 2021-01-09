8516ab216dcf4210e7ddaa2f88a1e25f1f9150fe
peyoot <peyoot@hotmail.com>
2021-01-09 00:30:51 -0600

    use fresh init hexo blog as mapping volumes

--- docker-github-pages/source/tags/index.md
+++
@@ -1,5 +0,0 @@
----
-title: tags
-date: 2020-07-10 20:17:42
-type: "tags"
----
---
+++ init_and_run.sh
@@ -0,0 +1,15 @@
+#git clone $PUBLIC_HEXO_GITHUB_URL
+#dir_git=${PUBLIC_HEXO_GITHUB_URL##*/}
+#cd ${dir_git%.*}
+#Please manually use git to manage your github project in host
+#cd ${HEXO_REPO}
+hexo init blog
+cd blog
+npm install
+#npm install hexo-deployer-git --save 
+#npm install hexo-render-pug hexo-renderer-stylus
+#npm install hexo-wordcount
+#git config --global user.name "peyoot"
+#git config --global user.email "peyoot@hotmail.com"
+hexo generate
+hexo server
